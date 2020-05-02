acd40b49ed9ee152ce282233c7e0a5fc04c126c1
unknown <Administrator@TRJ3573393.wks.internal.timbrasil.com.br>
2020-05-02 02:29:37 -0300

    middleware de autenticação

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '84d0e0d34d6ec0efb59780f31cfa8104',
+    expiresIn: '1d',
+  },
+};
--- src/config/authConfig.ts
+++
@@ -1,4 +0,0 @@
-export default {
-  secret: '84d0e0d34d6ec0efb59780f31cfa8104',
-  expiresIn: '1d',
-};
